ee29862951f92f3a0d168c00592d42c3b46e7f07
ArdaGurcan <ardagurcan2004@gmail.com>
2022-07-05 08:10:12 +0300

    add reticle and moving data

--- js/main.js
+++ js/main.js
@@ -23,17 +23,21 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.PlaneGeometry(1, 1);
 
-camera.position.z = 50;
+camera.position.z = 100;
 camera.position.y = 30;
 camera.position.x = 30;
+
+const tile = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial( 0x000000 ));
 
 // receive data
 connection.onmessage = function (event) {
     let data = JSON.parse(event.data);
+
     connection.close();
     let frame = 1;
+    console.log(data[1])
     let animate = () => {
 
         requestAnimationFrame(animate);
@@ -41,22 +45,35 @@ connection.onmessage = function (event) {
             for (let x = 0; x < data[frame]["info.width"]; x++) {
                 const material = new THREE.MeshBasicMaterial({
                     color: new THREE.Color(
-                        `hsl(${(100 - parseInt(
+                        `hsl(${(100 - parseFloat(
                         data[frame]["data_" +
                         (data[frame]["info.width"] * y + x)]
                       )) * 120 / 100},100%,70%)`
                     ),
                 });
-                const cube = new THREE.Mesh(geometry, material);
+                const cube = tile.clone()
+                cube.material = material
                 scene.add(cube);
-                cube.position.y = y;
-                cube.position.x = x;
+                cube.position.y = y + parseFloat(data[frame]["info.origin.position.y"]) / parseFloat(data[frame]["info.resolution"])
+                cube.position.x = x + parseFloat(data[frame]["info.origin.position.x"]) / parseFloat(data[frame]["info.resolution"])
+
+                // console.log(cube.position)
             }
         }
 
+        const reticle = new THREE.SphereGeometry(0.5, 32, 16)
+        const r = new THREE.Mesh(reticle, new THREE.MeshBasicMaterial( {color: 0x000000} ));
+        scene.add(r)
+        r.position.y = parseFloat(data[frame]["info.origin.position.y"]) / parseFloat(data[frame]["info.resolution"]) + 30
+        r.position.x = parseFloat(data[frame]["info.origin.position.x"]) / parseFloat(data[frame]["info.resolution"]) + 30
+
         renderer.render(scene, camera);
+        while (scene.children.length)
+        {
+            scene.remove(scene.children[0]);
+        }
         frame++
-        if (frame > 210) frame = 1
+        if (frame > 208) frame = 1
     };
     animate()
 };
